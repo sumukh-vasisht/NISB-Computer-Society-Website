27728744ce4df6547cc4de58650cb33975e6aa22
varun bheemaiah <varunbheemaiah9@gmail.com>
2019-03-02 00:37:28 +0530

    Added form validation

---
+++ static/js/validate.js
@@ -0,0 +1,19 @@
+function validate()
+{
+    var nameFormat = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]/;
+    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var Name = document.forms["Contact"]["contactName"].value;
+    var Email = document.forms["Contact"]["contactEmail"].value;
+    var Subject = document.forms["Contact"]["contactSubject"].value;
+    var Message = document.forms["Contact"]["contactMessage"].value;
+    if( nameFormat.test(Name) )
+    {
+        document.getElementById("validMessage").innerHTML = "Name cannot be blank or have special characters";
+        return false;
+    }
+    if( !mailFormat.test(Email) )
+    {
+        document.getElementById("validMessage").innerHTML = "Invalid e-mail entered";
+        return false;
+    }
+}
